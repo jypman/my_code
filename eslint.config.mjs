c5f0994e80c3ae7ffc18f4f33665b897e3600efb
jypman <jypark150695@gmail.com>
2026-01-17 16:21:41 +0900

    chore: 패키지 설치 및 환경설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores(['node_modules/**', '.next/**', 'build/**', 'next-env.d.ts', '.vscode/**']),
+  {
+    rules: {
+      'react/jsx-no-useless-fragment': 'warn',
+      'import/no-duplicates': 'error',
+      'jsx-a11y/alt-text': 'error',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'off',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
