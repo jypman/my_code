0fa4c0605c61ee2fc1f1ac075898098581f71e63
jypman <jypark150695@gmail.com>
2026-01-17 16:58:55 +0900

    feat: ssr을 위한 styled component 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import StyledComponentsRegistry from '@/lib/StyledComponentRegistry';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,9 @@ export default function RootLayout({
 }>): React.ReactElement {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
